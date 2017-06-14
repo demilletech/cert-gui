79efe864c946f5993058a38833eabf81c17fed45
Julian DeMille <jtdemille@demilletech.net>
2017-06-14 14:06:45 -0400

    Things

--- certutil.py
+++ certutil.py
@@ -1,5 +1,5 @@
-import OpenSSL.crypto
-from OpenSSL.crypto import load_certificate_request, FILETYPE_PEM
+from OpenSSL import crypto
+from OpenSSL.crypto import load_certificate_request, FILETYPE_PEM, X509, X509Req, PKey
 
 from os import listdir, popen
 from os.path import isfile, join
@@ -7,45 +7,37 @@ from os.path import isfile, join
 import certconfig
 import certsettings
 
+
 def createKey(params):
     command = certconfig.cd + "; " + certconfig.generateKey + "; " + certconfig.generateKeyPerms
     fcommand = certconfig.createCommand(command, params)
     stream = execute(fcommand)
 
+
 def genCSR(params):
     command = certconfig.cd + "; " + certconfig.generateCSR
     fcommand = certconfig.createCommand(command, params)
     stream = execute(fcommand)
-    
-def signCSR(cn):
-    csr = open(certsettings.settings['crlRequestsDir'] + cn + ".csr.pem")
-    req = load_certificate_request(FILETYPE_PEM, csr)
-    params = getCSRParams(req)
-    
-    command = certconfig.cd + "; " + certconfig.signCSR + "; " + certconfig.signCSRPerms
-    fcommand = certconfig.createCommand(command, params)
-    stream = execute(fcommand)
 
-def importCSR(csrText):
-    req = load_certificate_request(FILETYPE_PEM, csrText)
-    key_type = 'RSA' if key.type() == OpenSSL.crypto.TYPE_RSA else 'DSA'
-    components = getCSRParams(req)
-    
-    filename = components['CN'] + ".csr.pem"
-    file = open(filename)
-    file.write(csrText)
-    
-    return components['cn']
-    
+
+def signCSR(csr: X509Req, ca: PKey): return csr.sign(ca, b"sha256")
+
+
+def importCSR(csrText): return crypto.load_certificate_request(FILETYPE_PEM, csrText)
+    # return components['cn']
+
+
 def getCSRParams(cn):
     subject = req.get_subject()
     components = dict(subject.get_components())
     return components
 
+
 def getAllCSR():
     csrdir = certsettings.settings['certRequestsDir']
     onlyfiles = [f for f in listdir(csrdir) if isfile(join(csrdir, f))]
     return onlyfiles
+
 
 def execute(command):
     return os.popen(command)
